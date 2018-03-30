9851d08d7e03519c9a99733166b9bf8747f126e3
Matthew Wilson <matthew@productearthsolutions.com>
2018-03-30 14:29:01 +0100

    Adding support for N/Format module

---
+++ modules/SS2/NFormat.js
@@ -0,0 +1,43 @@
+module.exports = class NFormat {
+  constructor() {
+    this.Type = {
+      CCEXPDATE:"CCEXPDATE",
+      CCNUMBER:"CCNUMBER",
+      CCVALIDFROM:"CCVALIDFROM",
+      CHECKBOX:"CHECKBOX",
+      COLOR:"COLOR",
+      CURRENCY:"CURRENCY",
+      CURRENCY2:"CURRENCY2",
+      DATE:"DATE",
+      DATETIME:"DATETIME",
+      DATETIMETZ:"DATETIMETZ",
+      FLOAT:"FLOAT",
+      FULLPHONE:"FULLPHONE",
+      FUNCTION:"FUNCTION",
+      IDENTIFIER:"IDENTIFIER",
+      INTEGER:"INTEGER",
+      MMYYDATE:"MMYYDATE",
+      NONNEGCURRENCY:"NONNEGCURRENCY",
+      NONNEGFLOAT:"NONNEGFLOAT",
+      PERCENT:"PERCENT",
+      PHONE:"PHONE",
+      POSCURRENCY:"POSCURRENCY",
+      POSFLOAT:"POSFLOAT",
+      POSINTEGER:"POSINTEGER",
+      RATE:"RATE",
+      RATEHIGHPRECISION:"RATEHIGHPRECISION",
+      TIME:"TIME",
+      TIMEOFDAY:"TIMEOFDAY",
+      TIMETRACK:"TIMETRACK",
+      URL:"URL"
+    }
+  }
+
+  format(options){
+    return options.value.toString()
+  }
+
+  parse(options){
+    return options.value;
+  }
+}
